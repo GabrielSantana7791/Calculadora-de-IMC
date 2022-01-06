c882692aa547d1ca951c1d934e962aca66d21106
Gabriel de Paula <gabrielsantana7791@gmail.com>
2022-01-05 21:49:49 -0300

    init

---
+++ app/src/main/java/com/gabrielsoft/calculadoradeimc/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gabrielsoft.calculadoradeimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerTelaInicial, new TelaInicial());
+        fragmentTransaction.commit();
+    }
+
+
+}
